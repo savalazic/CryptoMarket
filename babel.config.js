aca8bfbab8d180e8d7ba520d61917ed955b97a85
Sava Lazic <savalazic9@gmail.com>
2019-03-02 16:46:17 +0100

    setup tools

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"]
-}
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@services': './src/services',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+};
