1865323eac96720dc5f6e6405847258aea1a7390
Sava Lazic <savalazic9@gmail.com>
2019-03-07 18:54:33 +0100

    disable yellow box for web socket

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+/* eslint-disable */
 /**
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
 import { AppRegistry } from 'react-native';
+import { YellowBox } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
+
+console.ignoredYellowBox = ['Remote debugger'];
+YellowBox.ignoreWarnings([
+    'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
 
 AppRegistry.registerComponent(appName, () => App);
